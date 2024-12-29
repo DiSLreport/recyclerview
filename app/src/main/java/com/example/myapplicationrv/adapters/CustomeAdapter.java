2241df93594348b485dc7e9d454a8a9dbe1ab812
Lior DiSL hanina <deathisself@gmail.com>
2024-12-30 00:29:14 +0200

    test

---
+++ app/src/main/java/com/example/myapplicationrv/adapters/CustomeAdapter.java
@@ -0,0 +1,73 @@
+package com.example.myapplicationrv.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplicationrv.R;
+import com.example.myapplicationrv.models.Data;
+
+import java.util.ArrayList;
+
+public class CustomeAdapter extends RecyclerView.Adapter<CustomeAdapter.myViewHolder> {
+
+    private ArrayList<Data> arr;
+
+    public CustomeAdapter(ArrayList<Data> arr) {
+
+        this.arr = arr;
+    }
+    public class myViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView username;
+        TextView nameVersion;
+        ImageView imageViewItem;
+
+        public myViewHolder ( View itemView){
+            super(itemView);
+           username = itemView.findViewById(R.id.textName);
+           nameVersion = itemView.findViewById(R.id.textVer);
+           imageViewItem = itemView.findViewById(R.id.imageView);
+        }
+
+    }
+    @NonNull
+    @Override
+    public CustomeAdapter.myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview , parent , false ) ;
+
+        myViewHolder MyViewHolder = new myViewHolder(view);
+
+       return MyViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomeAdapter.myViewHolder holder, int position) {
+        holder.username.setText(arr.get(position).getName());
+        holder.nameVersion.setText(arr.get(position).getVersion());
+        holder.imageViewItem.setImageResource(arr.get(position).getImage());
+
+        // Set an OnClickListener to the entire itemView
+        holder.itemView.setOnClickListener(view -> {
+            // Show message
+            String message =  arr.get(position).getName() + "\n"+ arr.get(position).getVersion();
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arr.size();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplicationrv/classes/myData.java
@@ -0,0 +1,34 @@
+package com.example.myapplicationrv.classes;
+
+
+import com.example.myapplicationrv.R;
+
+public class myData {
+
+    public static String[] nameArray = {"bulbasaur 001", "charmander 004", "squirtle 007","butterfree 014","ekans 023","pikachu 025","vulpix 037","jigglypuff 039",
+            "abra 063","machop 066","geodude 074","magnemite 081","cubone 104","eevee 133","articuno 144","dratini 147"};
+    public static String[] versionArray = {"A Grass/Poison-type Pokémon with a plant bulb on its back that grows into a large flower.",
+            "A Fire-type Pokémon resembling a small dinosaur, with a flame at the tip of its tail.",
+            " Water-type turtle Pokémon known for its hard shell and ability to shoot water from its mouth.",
+            "A Bug/Flying-type Pokémon that evolves from Metapod and can fly gracefully using its large wings.",
+            "A Poison-type Pokémon resembling a snake, known for its ability to coil and constrict its prey.",
+            "An Electric-type Pokémon and mascot of the franchise, known for its yellow fur and lightning bolt-shaped tail.",
+            "A Fire-type fox Pokémon with six fiery tails that grow stronger with age.",
+            "A Fairy-type Pokémon that sings lullabies to put others to sleep, inflating its body to look larger.",
+            "A Psychic-type Pokémon known for its teleportation abilities, often escaping battles with ease.",
+            "A Fighting-type Pokémon that is strong and loves to train to become even stronger.",
+            " A Rock/Ground-type Pokémon with a tough, rocky body and powerful arms used for lifting boulders.",
+            " An Electric/Steel-type Pokémon with magnet-like abilities that can attract steel objects.",
+            "A Ground-type Pokémon that wears its deceased mother’s skull as a helmet, mourning her loss.",
+            "A Normal-type Pokémon with multiple evolutions, each adapting to different environments and elements.",
+            "A Legendary Ice/Flying-type Pokémon known for its powerful ice attacks and majestic appearance.",
+            "A Dragon-type Pokémon that evolves into Dragonair and is often found in rivers and lakes."};
+
+    public static Integer[] drawableArray = {R.drawable.bulbasaur_001, R.drawable.charmander_004, R.drawable.squirtle_007,
+            R.drawable.butterfree_014, R.drawable.ekans_023, R.drawable.pikachu_025, R.drawable.vulpix_037,R.drawable.jigglypuff_039,R.drawable.abra_063,
+            R.drawable.machop_066,R.drawable.geodude_074,R.drawable.magnemite_081, R.drawable.cubone_104, R.drawable.eevee_133,R.drawable.articuno_144,
+            R.drawable.dratini_147};
+
+    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15};
+
+}
